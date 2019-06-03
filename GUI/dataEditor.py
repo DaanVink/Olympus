8aed3b25181abf4997500e20f2a61e7bcbb91a06
Davin <davinmiler@gmail.com>
2019-06-03 13:13:39 +0200

    Working on documentation

---
+++ GUI/dataEditor.py
@@ -0,0 +1,176 @@
+from PyQt5 import QtGui, QtWidgets, QtCore, uic
+from GUI import confirm 
+
+class TypeEditor(QtWidgets.QDialog):
+    def __init__(self, db, types, parent=None):
+        super().__init__(parent)
+        uic.loadUi("GUI/typeEditor.ui", self)
+
+        self.initialName = ""
+        self.initialColor = ""
+        self.newName = ""
+        self.newColor = ""
+        self.lastSelection = 0
+
+        self.db = db
+        self.cursor = self.db.cursor()
+
+        for x in types:
+            self.listWidget.addItem(x)
+
+        self.listWidget.itemSelectionChanged.connect(self.update)
+        self.listWidget.setCurrentRow(0)
+
+        self.buttonCancel.clicked.connect(self.cancel)
+        self.buttonApply.clicked.connect(self.apply)
+        self.buttonConfirm.clicked.connect(self.confirm)
+        self.buttonColor.clicked.connect(self.openColorDialog)
+        self.fieldName.textChanged.connect(self.onTextChanged)
+
+        self.show()
+
+    def cancel(self):
+        if self.initialColor != self.newColor or self.initialName != self.newName:
+            prompt = confirm.confirm(text="You have unsaved work. Are you sure you want to quit?", title=" ")
+            if prompt.response:
+                self.close()
+            else:
+                pass
+        else:
+            self.close()
+
+    def apply(self):
+        self.initialName = self.newName
+        self.initialColor = self.newColor
+        self.cursor.execute(f"UPDATE types SET color = '{self.newColor}', name = '{self.newName}' WHERE name = '{self.initialName}'")
+        if (self.initialName != self.newName):
+            self.cursor.execute(f"UPDATE data SET type = '{self.newName}' WHERE type = '{self.initialName}'")
+        self.db.commit()
+    
+    def confirm(self):
+        self.cursor.execute(f"UPDATE types SET color = '{self.newColor}', name = '{self.newName}' WHERE name = '{self.initialName}'")
+        if (self.initialName != self.newName):
+            self.cursor.execute(f"UPDATE data SET type = '{self.newName}' WHERE type = '{self.initialName}'")
+        self.db.commit()
+        self.close()
+    
+    def openColorDialog(self):
+        color = QtWidgets.QColorDialog.getColor()
+
+        if color.isValid():
+            self.newColor = color.name()
+            self.buttonColor.setStyleSheet(f"background-color: {self.newColor}" )
+    
+    def onTextChanged(self):
+        self.newName = self.fieldName.text()
+    
+    def update(self):
+        if self.initialColor != self.newColor or self.initialName != self.newName:
+            prompt = confirm.confirm(text="You have unsaved work. Are you sure you want to quit?", title=" ")
+            if not prompt.response:
+                self.listWidget.blockSignals(True)
+                self.listWidget.setCurrentRow(self.lastSelection)
+                self.listWidget.blockSignals(False)
+                return
+        self.lastSelection = self.listWidget.currentRow()
+        selected = self.listWidget.currentItem()
+
+        name = selected.data(0)
+        self.cursor.execute(f"SELECT color FROM types WHERE name = '{name}'")
+        color = self.cursor.fetchall()
+
+        self.initialName = str(name)
+        self.initialColor = str(color[0][0])
+
+        self.newName = self.initialName
+        self.newColor = self.initialColor
+
+        self.fieldName.setText(name)
+        self.buttonColor.setStyleSheet(f"background-color: {self.initialColor}" )
+    
+class CategoryEditor(QtWidgets.QDialog):
+    def __init__(self, db, types, parent=None):
+        super().__init__(parent)
+        uic.loadUi("GUI/typeEditor.ui", self)
+
+        self.initialName = ""
+        self.initialColor = ""
+        self.newName = ""
+        self.newColor = ""
+        self.lastSelection = 0
+
+        self.db = db
+        self.cursor = self.db.cursor()
+
+        for x in types:
+            self.listWidget.addItem(x)
+
+        self.listWidget.itemSelectionChanged.connect(self.update)
+        self.listWidget.setCurrentRow(0)
+
+        self.buttonCancel.clicked.connect(self.cancel)
+        self.buttonApply.clicked.connect(self.apply)
+        self.buttonConfirm.clicked.connect(self.confirm)
+        self.buttonColor.clicked.connect(self.openColorDialog)
+        self.fieldName.textChanged.connect(self.onTextChanged)
+
+        self.show()
+
+    def cancel(self):
+        if self.initialColor != self.newColor or self.initialName != self.newName:
+            prompt = confirm.confirm(text="You have unsaved work. Are you sure you want to quit?", title=" ")
+            if prompt.response:
+                self.close()
+            else:
+                pass
+        else:
+            self.close()
+
+    def apply(self):
+        self.initialName = self.newName
+        self.initialColor = self.newColor
+        self.cursor.execute(f"UPDATE types SET color = '{self.newColor}', name = '{self.newName}' WHERE name = '{self.initialName}'")
+        if (self.initialName != self.newName):
+            self.cursor.execute(f"UPDATE data SET type = '{self.newName}' WHERE type = '{self.initialName}'")
+        self.db.commit()
+    
+    def confirm(self):
+        self.cursor.execute(f"UPDATE types SET color = '{self.newColor}', name = '{self.newName}' WHERE name = '{self.initialName}'")
+        if (self.initialName != self.newName):
+            self.cursor.execute(f"UPDATE data SET type = '{self.newName}' WHERE type = '{self.initialName}'")
+        self.db.commit()
+        self.close()
+    
+    def openColorDialog(self):
+        color = QtWidgets.QColorDialog.getColor()
+
+        if color.isValid():
+            self.newColor = color.name()
+            self.buttonColor.setStyleSheet(f"background-color: {self.newColor}" )
+    
+    def onTextChanged(self):
+        self.newName = self.fieldName.text()
+    
+    def update(self):
+        if self.initialColor != self.newColor or self.initialName != self.newName:
+            prompt = confirm.confirm(text="You have unsaved work. Are you sure you want to quit?", title=" ")
+            if not prompt.response:
+                self.listWidget.blockSignals(True)
+                self.listWidget.setCurrentRow(self.lastSelection)
+                self.listWidget.blockSignals(False)
+                return
+        self.lastSelection = self.listWidget.currentRow()
+        selected = self.listWidget.currentItem()
+
+        name = selected.data(0)
+        self.cursor.execute(f"SELECT color FROM types WHERE name = '{name}'")
+        color = self.cursor.fetchall()
+
+        self.initialName = str(name)
+        self.initialColor = str(color[0][0])
+
+        self.newName = self.initialName
+        self.newColor = self.initialColor
+
+        self.fieldName.setText(name)
+        self.buttonColor.setStyleSheet(f"background-color: {self.initialColor}" )
--- main.py
+++ main.py
@@ -3,11 +3,12 @@
 #TODO: clean up functions and group related code using whitespace
 #TODO: comments
 #TODO: add detail view for categories with "Rename category", "Delete", etc
+#TODO: write a README.MD
 
 from PyQt5 import QtGui, QtWidgets, QtCore, uic
 import sys, json, sqlite3
 
-from GUI import contentViews, typeEditor
+from GUI import contentViews, dataEditor
 import settingsHandler
 
 db = sqlite3.connect("data/mainStore.sqlite")
@@ -26,55 +27,69 @@ class MainWindow(QtWidgets.QMainWindow):
         self.cursor = self.db.cursor()
 
         self.data = {}
+        # This query gets all data to be shown in the tree from the DB
         for category in settingsHolder.settings["categories"]:
             query = f"SELECT name, content, type, id FROM data WHERE category == '{category}'"
             self.cursor.execute(query)
             self.data[str(category)] = self.cursor.fetchall()
 
-        self.categoryFilter.addItem("")
+        self.categoryFilter.addItem("") # Add an empty item to the start of the combobox, signifying no filter
         for item in settingsHolder.settings["categories"]:
-            self.categoryFilter.addItem(item)
-        self.categoryFilter.activated.connect(self.catFilterFunc)
+            self.categoryFilter.addItem(item) # Add every category to the combobox
+        self.categoryFilter.activated.connect(self.catFilterActivated) # Call a function to update the tree according to the new filters
 
-        self.typeFilter.addItem("")
+        self.typeFilter.addItem("") # Add an empty item to the start of the combobox, signifying no filter
         for item in settingsHolder.settings["types"]:
-            self.typeFilter.addItem(item)
-        self.typeFilter.activated.connect(self.typeFilterFunc)
+            self.typeFilter.addItem(item) # Add every type to the combobox
+        self.typeFilter.activated.connect(self.typeFilterActivated) # Call a function to update the tree according to the new filters
 
-        self.searchBox.textChanged.connect(self.onSearchChange)
+        self.searchBox.textChanged.connect(self.searchboxChange)
         self.filter = {"category": "", "type": "", "search": ""}
 
-        self.treeView.setColumnWidth(0, 150)
-        self.treeView.setAlternatingRowColors(False)
+        self.treeView.setColumnWidth(0, 150) # This should be a settings when i get around to adding proper settings
+        self.treeView.setAlternatingRowColors(False) # This too
         self.treeView.expandAll()
-        self.treeView.doubleClicked.connect(self.onDoubleClick)
-        self.updateTree()
-
+        self.treeView.doubleClicked.connect(self.treeDoubleClick)
+        self.updateTree() # This function holds all code responsible for editing the data shown in the tree
+        
+        # Setting sup the menubar
         self.menu = self.menuBar()
         self.addNew = QtWidgets.QAction("New", self)
         self.addNew.triggered.connect(self.addContent)
         self.menu.addAction(self.addNew)
 
+        self.dataMenu = QtWidgets.QMenu("Data", self)
+        self.menu.addMenu(self.dataMenu)
+
         self.editTypes = QtWidgets.QAction("Types", self)
         self.editTypes.triggered.connect(self.editTypesFunc)
-        self.menu.addAction(self.editTypes)
+        self.dataMenu.addAction(self.editTypes)
+
+        # For now, this is just a copy of the type editor
+        self.editCategories = QtWidgets.QAction("Categories", self)
+        self.editCategories.triggered.connect(self.editCategoriesFunc)
+        self.dataMenu.addAction(self.editCategories)
 
         self.tmp = QtWidgets.QAction("Debug", self)
-        self.tmp.triggered.connect(self.tmpFunc)
+        self.tmp.triggered.connect(self.debugFunction)
         self.menu.addAction(self.tmp)
+
+        # Final actions before showing the window
 
         self.setWindowTitle("Project Olympus")
         self.show()
 
     def updateTree(self):
-
+        # First we build up a query to load data for the DB
+        # This query is made up of three parts: the base, the category filter and the type filter
+        # The type filter is stored in the addition variable before being added to the base query
         addition = ""
-        if self.filter["category"] is not "":
+        if self.filter["category"] is not "": # Check if we need to filter
             filteredCategories = [ self.filter["category"] ]
         else:
             filteredCategories = settingsHolder.settings["categories"]
         
-        filterType = self.filter["type"] if self.filter["type"] is not "" else ""
+        filterType = self.filter["type"] if self.filter["type"] is not "" else "" # we need to create a new variable here because f-strings can only take pure variables, not list indices
         if filterType is not "":
             addition += f" AND type = '{filterType}'"
         if self.filter["search"] is not "":
@@ -82,17 +97,21 @@ class MainWindow(QtWidgets.QMainWindow):
             addition += f" AND (name LIKE '%{ search }%' or content LIKE '%{ search }%')"
 
         self.data = {}
+        # For every category, create a query string and execute it. Then store the resulting data in a dictionary with as key the category
         for category in filteredCategories:
             query = f"SELECT name, content, type, id FROM data WHERE category = '{category}'" + addition
             self.cursor.execute(query)
             self.data[str(category)] = self.cursor.fetchall()
 
+        # In Qt, a treeview requires a Model to store the data
         self.model = QtGui.QStandardItemModel()
         self.model.setColumnCount(4)
         self.model.setHorizontalHeaderLabels(["Title", "Notes", "Type", "ID"])
 
         self.rootNode = self.model.invisibleRootItem()
 
+        # This part deals with adding the data to the newly created Model
+        # Every category is represented by a new branch
         self.branches = []
         for x in range(0, len(filteredCategories)):
             category = filteredCategories[x]
@@ -100,19 +119,12 @@ class MainWindow(QtWidgets.QMainWindow):
             for node in range(0, len(self.data[str(category)])):
                 currentData = self.data[str(category)][node]
                 color = QtGui.QColor(settingsHolder.settings["typeColors"][currentData[2]])
-                obj0 = QtGui.QStandardItem(str(currentData[0]))
-                obj0.setForeground(color)
-                obj1 = QtGui.QStandardItem(str(currentData[1]))
-                obj1.setForeground(color)
-                obj2 = QtGui.QStandardItem(str(currentData[2]))
-                obj2.setForeground(color)
-                obj3 = QtGui.QStandardItem(str(currentData[3]))
-                obj3.setForeground(color)
-                self.branches[x].appendRow([obj0,
-                                            obj1,
-                                            obj2,
-                                            obj3
-                                            ])
+                row = []
+                for y in range(0, 4):
+                    tmp = QtGui.QStandardItem(str(currentData[y]))
+                    tmp.setForeground(color)
+                    row.append(tmp)
+                self.branches[x].appendRow(row)
         
         for branch in self.branches:
             self.rootNode.appendRow( [branch] )
@@ -121,47 +133,45 @@ class MainWindow(QtWidgets.QMainWindow):
         self.treeView.expandAll()
     
     def addContent(self):
-        self.new = contentViews.AddContent(settingsHolder)
-        self.new.buttonApply.clicked.connect(self.retrieveNewContent)
+        self.new = contentViews.AddContent(settingsHolder, db)
+        self.new.exec() # block thread until dialog closes
+        self.updateTree()
     
-    def retrieveNewContent(self):
-        d = self.new.obj
-        try:
-            self.cursor.execute("INSERT INTO data(name, content, category, type) VALUES(?,?,?,?)", [d["title"], d["notes"], d["category"], d["type"]])
-            self.db.commit()
-            self.updateTree()
-        except sqlite3.IntegrityError as e:
-            print("Not a unique ID")
-            print(e)
-    
-    def catFilterFunc(self, index):
+    def catFilterActivated(self, index):
         self.filter["category"] = self.categoryFilter.itemText(index)
         self.updateTree()
 
-    def typeFilterFunc(self, index):
+    def typeFilterActivated(self, index):
         self.filter["type"] = self.typeFilter.itemText(index)
         self.updateTree()
     
-    def onSearchChange(self):
+    def searchboxChange(self):
         search = self.searchBox.text()
         self.filter["search"] = search
         self.updateTree()
 
-    def onDoubleClick(self, index):
+    def treeDoubleClick(self, index):
         id = self.treeView.selectedIndexes()[3].data()
         if id is None:
             pass
         else:
-            self.detail = contentViews.ViewContent(settingsHolder, id, self.db, self.cursor)
+            self.detail = contentViews.ViewContent(settingsHolder, id, self.db)
+            self.detail.exec() # block thread until dialog closes
         self.updateTree()
     
     def editTypesFunc(self):
-        self.new = typeEditor.Editor(self.db, self.cursor, settingsHolder.settings["types"])
-        self.new.exec() # wait for the dialog to return
-        settingsHolder.load()
+        self.new = dataEditor.TypeEditor(self.db, settingsHolder.settings["types"])
+        self.new.exec() # block thread until dialog closes
+        settingsHolder.load() # reload settings
         self.updateTree()
     
-    def tmpFunc(self):
+    def editCategoriesFunc(self):
+        self.new = dataEditor.CategoryEditor(self.db, settingsHolder.settings["types"])
+        self.new.exec() # block thread until dialog closes
+        settingsHolder.load() # reload settings
+        self.updateTree()
+    
+    def debugFunction(self):
         pass
 
 main = MainWindow(db)
